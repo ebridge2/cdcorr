6965d1310dea47d5426a61953a751be23eabbadb
Eric W. Bridgeford <ebridge2@jhu.edu>
2023-01-25 23:13:20 -0500

    updated simulations

---
+++ cdcorr/simulations/sims.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import statsmodels
+import dodiscover as dod
+import hyppo
+import scipy as sp
+import sklearn as sk
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+def simulate_covars(causal_preds, balance=1):
+    coefs = []
+    #for causal_pred in causal_preds:
+    #    if causal_pred == 0:
+    #        coefs.append((3, 3*1/balance))
+    #    else:
+    #        coefs.append((3*1/balance, 3))
+    #covars = 2*np.array([float(np.random.beta(alpha, beta, size=1)) for (alpha, beta) in coefs]) - 1
+    balance_id = np.random.binomial(1, balance, size=len(causal_preds))
+    
+    for causal_cl, bal_id in zip(causal_preds, balance_id):
+        if bal_id == 1:
+            coefs.append((10, 10))
+        else:
+            if causal_cl == 0:
+                coefs.append((2, 8))
+            else:
+                coefs.append((8, 2))
+    covars = 2*np.array([float(np.random.beta(alpha, beta, size=1)) for (alpha, beta) in coefs]) - 1
+    return(covars)
+
+def simulate_covars_multiclass(causal_preds, balance=1):
+    coefs = []
+    balance_id = np.random.binomial(1, balance, size=len(causal_preds))
+    
+    for causal_cl, bal_id in zip(causal_preds, balance_id):
+        if bal_id == 1:
+            coefs.append((4, 2))
+        else:
+            if causal_cl == 0:
+                coefs.append((2, 8))
+            else:
+                coefs.append((8, 2))
+    covars = 2*np.array([float(np.random.beta(alpha, beta, size=1)) for (alpha, beta) in coefs]) - 1
+    return(covars)
+    
+def sigmoidal_sim(n, p, balance=1, causal_effect_size=1, pi=0.5, err_scale = 0.5):
+    Ts = np.random.binomial(1, pi, size=n)
+    Xs = simulate_covars(Ts, balance=balance)
+    y_base = sigmoid(8*Xs).reshape(n, 1)
+    Bs = np.sqrt(np.linspace(1/np.sqrt(p), 1/(p**2), p).reshape(1, p))
+    Ys_covar = 2*causal_effect_size*y_base @ Bs
+    Ys_causal = -causal_effect_size * Ts.reshape(n, 1) @ Bs
+    err = np.random.normal(scale=err_scale, size=(n, p)).reshape(n, p)
+    Ys =  Ys_covar + Ys_causal + err
+    
+    # true signal at a given x
+    Ntrue = 200
+    true_x = np.linspace(-1, 1, int(Ntrue/2))
+    true_x = np.concatenate((true_x, true_x))
+    true_y_base = sigmoid(8*true_x).reshape(Ntrue, 1)
+    true_y_covar = 2*causal_effect_size*true_y_base @ Bs
+    true_t = np.concatenate((np.zeros(int(Ntrue/2)), np.ones(int(Ntrue/2)))).astype(int)
+    true_y_causal = -causal_effect_size * true_t.reshape(Ntrue, 1) @ Bs
+    true_y = true_y_covar + true_y_causal
+    return(Ys, Ts, Xs, true_y, true_t, true_x)
+
+def linear_sim(n, p, balance=1, causal_effect_size=1, pi=0.5, err_scale = 0.5):
+    Ts = np.random.binomial(1, pi, size=n)
+    Xs = simulate_covars(Ts, balance=balance)
+    y_base = Xs.reshape(-1, 1)
+    Bs = np.sqrt(np.linspace(1/np.sqrt(p), 1/(p**2), p).reshape(1, p))
+    Ys_covar = 2*causal_effect_size*y_base @ Bs
+    Ys_causal = -causal_effect_size * Ts.reshape(n, 1) @ Bs
+    err = np.random.normal(scale=err_scale, size=(n, p)).reshape(n, p)
+    Ys =  Ys_covar + Ys_causal + err
+    
+    # true signal at a given x
+    Ntrue = 200
+    true_x = np.linspace(-1, 1, int(Ntrue/2))
+    true_x = np.concatenate((true_x, true_x))
+    true_y_base = true_x.reshape(Ntrue, 1)
+    true_y_covar = 2*causal_effect_size*true_y_base @ Bs
+    true_t = np.concatenate((np.zeros(int(Ntrue/2)), np.ones(int(Ntrue/2)))).astype(int)
+    true_y_causal = -causal_effect_size * true_t.reshape(Ntrue, 1) @ Bs
+    true_y = true_y_covar + true_y_causal
+    return(Ys, Ts, Xs, true_y, true_t, true_x)
+
+def kclass_sim(n, p, balance=1, causal_effect_size=1, covar_effect_size=1, pi=0.5, err_scale = 0.5, K=3):
+    Ts = np.random.choice(range(0, K), size=n, p=np.concatenate(([pi], (1-pi)*1/(K-1)*np.ones((K-1)))))
+    Xs = simulate_covars_multiclass(Ts, balance=balance)
+    y_base = sigmoid(8*Xs).reshape(n, 1)
+    Bs = np.sqrt(np.linspace(1/np.sqrt(p), 1/(p**2), p).reshape(1, p))
+    Ys_covar = 2*causal_effect_size*y_base @ Bs
+    Ys_causal = -causal_effect_size * (Ts != 0).astype(float).reshape(n, 1) @ Bs
+    err = np.random.normal(scale=err_scale, size=(n, p)).reshape(n, p)
+    Ys =  Ys_covar + Ys_causal + err
+    
+    # true signal at a given x
+    Ntrue = K*100
+    true_x = np.linspace(-1, 1, int(Ntrue/K))
+    true_x = np.concatenate([true_x for k in range(0, K)])
+    true_y_base = sigmoid(8*true_x).reshape(Ntrue, 1)
+    true_y_covar = 2*causal_effect_size*true_y_base @ Bs
+    true_t = np.concatenate([np.zeros(int(Ntrue/K)) + k for k in range(0, K)]).astype(int)
+    true_y_causal = -causal_effect_size * (true_t != 0).astype(float).reshape(Ntrue, 1) @ Bs
+    true_y = true_y_covar + true_y_causal
+    return(Ys, Ts, Xs, true_y, true_t, true_x)
+
+def cond_manova(Y, T, X, **kwargs):
+    m_man = sm.multivariate.MANOVA(Y, np.vstack((X, T, np.ones(Y.shape[0]))).transpose())
+    testout = m_man.mv_test()
+    pbt = testout.results["x1"]["stat"].iloc[1]
+    stat, pval = (pbt[0], pbt[4])
+    return pval, stat
+
+def codite(Y, T, X, nrep=1000):
+    df_dict = {"Covariate" : X, "Batch" : T}
+    yvars = []
+    for i in range(0, Y.shape[1]):
+        yvar = "Y{:d}".format(i)
+        df_dict[yvar] = Y[:,i]
+        yvars.append(yvar)
+    df = pd.DataFrame(df_dict)
+    xvars = "Covariate"; group_col="Batch"
+    stat, pval = dod.cd.KernelCDTest(null_reps=nrep).test(df, [xvars], yvars, group_col)
+    return pval, stat
+    
+def cond_dcorr(Y, T, X, nrep=1000):
+    DT = sk.metrics.pairwise_distances(ohe(T), metric="l2")
+    DY = sk.metrics.pairwise_distances(Y, metric="l2")
+    if len(X.shape) == 1:
+        X = X.reshape(-1, 1)
+    DX = sk.metrics.pairwise_distances(X, metric="l2")
+    stat, pval = hyppo.conditional.CDcorr(compute_distance=None).test(DY, DT, DX, reps=nrep)
+    return pval, stat
+    
+def causal_prep(Xs, Ts):
+    # adopted from Lopez 2017 Matching to estimate the causal effect
+    # from multiple treatments
+    Xs = sm.add_constant(Xs)
+    m = sm.MNLogit(Ts, Xs)
+
+    fit = m.fit()
+    pred = fit.predict(Xs)
+
+    Ts_unique = np.unique(Ts)
+    K = len(Ts_unique)
+    Rtable = []
+    # check possible predictions T
+    for T in range(0, K):
+        Rtab = np.zeros((2, K))
+        # for each prediction T, check what elements
+        # which are in class Tp, and look at prediction
+        # probability for class T
+        for Tp in range(0, K):
+            Rtab[0,Tp] = pred[Ts == Tp,T].min()
+            Rtab[1,Tp] = pred[Ts == Tp,T].max()
+        # low and high are the max of the mins and the min of the maxes
+        # for class T
+        Rtable.append(np.array((Rtab[0,:].max(), Rtab[1,:].min())))
+
+    balance_check = np.zeros((Xs.shape[0], K))
+    for T in range(0, K):
+        for i in range(0, Xs.shape[0]):
+            balance_check[i, T] = pred[i, T] >= Rtable[T][0] and pred[i, T] <= Rtable[T][1]
+    balanced_ids = balance_check.all(axis=1)
+    return(balanced_ids)
+
+def ohe(T):
+    K = len(np.unique(T))
+    ohe_dat = np.zeros((len(T), K))
+    for t in np.unique(T):
+        ohe_dat[:,t] = (T == t).astype(int)
+    return ohe_dat
