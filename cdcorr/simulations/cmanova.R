0f8b53422f2470b8a78d81fc03da5dc2427dc18e
Eric W. Bridgeford <ebridge2@jhu.edu>
2023-02-13 14:32:29 -0500

    updated cmanova R

--- cdcorr/simulations/cmanova.R
+++ cdcorr/simulations/cmanova.R
@@ -9,15 +9,15 @@ cmanova <- function(Y, G, X) {
   test <- stats:::anova.mlmlist(mod_alt, mod_null)
   return(list(stat=test$Pillai[2], pvalue=test$`Pr(>F)`[2]))
 }
-
-Yn <- as.matrix(read.csv('./data/sigsim_Y_null.csv', header=FALSE, sep= " "))
-Xn <- as.matrix(read.csv('./data/sigsim_X_null.csv', header=FALSE, sep= " "))
-Gn <- factor(as.matrix(read.csv('./data/sigsim_T_null.csv', header=FALSE, sep= " ")))
 # 
-Y <- as.matrix(read.csv('./data/sigsim_Y_effect.csv', header=FALSE, sep= " "))
-X <- as.matrix(read.csv('./data/sigsim_X_effect.csv', header=FALSE, sep= " "))
-G <- as.matrix(read.csv('./data/sigsim_T_effect.csv', header=FALSE, sep= " "))
-# 
-cmanova(Yn, Gn, Xn)
-cmanova(Y, G, X)
+# Yn <- as.matrix(read.csv('./data/sigsim_Y_null.csv', header=FALSE, sep= " "))
+# Xn <- as.matrix(read.csv('./data/sigsim_X_null.csv', header=FALSE, sep= " "))
+# Gn <- factor(as.matrix(read.csv('./data/sigsim_T_null.csv', header=FALSE, sep= " ")))
+# # 
+# Y <- as.matrix(read.csv('./data/sigsim_Y_effect.csv', header=FALSE, sep= " "))
+# X <- as.matrix(read.csv('./data/sigsim_X_effect.csv', header=FALSE, sep= " "))
+# G <- as.matrix(read.csv('./data/sigsim_T_effect.csv', header=FALSE, sep= " "))
+# # 
+# cmanova(Yn, Gn, Xn)
+# cmanova(Y, G, X)
 
